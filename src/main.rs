bd8d52e7ce027bbcfd5b721a2ef70574036c77ba
Asur <asur@asurbernardo.com>
2019-11-20 15:25:02 +0100

    Make API backend respond correctly when requested from form

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,44 @@
-use actix_web::{web, middleware, App, HttpResponse, HttpServer, Responder, Result};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+#[macro_use] extern crate cached;
+
 use actix_files::NamedFile;
+use validator::Validate;
+use actix_web::{web, middleware, guard, http, App, HttpResponse, HttpServer, Result};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Validate, Serialize, Deserialize)]
+struct Request {
+    #[validate(length(min = "1", max = "1000000"))]
+    domain: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct Response {
+    supported: bool
+}
 
 fn index() -> Result<NamedFile> {
     Ok(NamedFile::open("static/index.html")?)
 }
 
-fn support() -> impl Responder {
-    HttpResponse::Ok().body("Supports HTTP/3!")
+fn p404() -> Result<NamedFile> {
+    Ok(NamedFile::open("static/404.html")?.set_status_code(http::StatusCode::NOT_FOUND))
+}
+
+fn support(
+    request: web::Json<Request>
+) -> HttpResponse {
+    let domain = &request.domain;
+    HttpResponse::Ok().json(Response {
+        supported: check(domain.to_string())
+    })
+}
+
+cached!{
+    CHECK;
+    fn check(domain: String) -> bool = {
+        true
+    }
 }
 
 fn main() {
@@ -19,14 +50,29 @@ fn main() {
     builder.set_private_key_file("key.pem", SslFiletype::PEM).unwrap();
     builder.set_certificate_chain_file("cert.pem").unwrap();
 
+    let endpoint = "127.0.0.1:8080";
+
+    println!("Starting server at: {:?}", endpoint);
+
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::NormalizePath)
             .wrap(middleware::Compress::default())
-            .route("/", web::get().to(index))
-            .route("/support", web::get().to(support))
+            .route("/", web::get().to_async(index))
+            .route("/support", web::post().to_async(support))
+            .default_service(
+                // 404 for GET request
+                web::resource("")
+                    .route(web::get().to(p404))
+                    // All requests that are not `GET`
+                    .route(
+                        web::route()
+                            .guard(guard::Not(guard::Get()))
+                            .to(HttpResponse::MethodNotAllowed),
+                    ),
+            )
     })
-    .bind_ssl("127.0.0.1:8088", builder)
+    .bind_ssl(endpoint, builder)
     .unwrap()
     .run()
     .unwrap();
